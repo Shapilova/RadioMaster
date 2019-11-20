a784892764617c0618711f737b35f4fc96db1291
Fiarcherry <fiarchersschannel@gmail.com>
2019-11-20 13:30:01 +0300

    Добавлены комментарии

--- RadioMaster/Controllers/ItemController.cs
+++ RadioMaster/Controllers/ItemController.cs
@@ -3,27 +3,32 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace RadioMaster.Controllers
 {
+    //Контроллер товаров
     public class ItemController : Controller
     {
         // GET: Item
+        //Отображение товаров
         public ActionResult Index()
         {
             return View();
         }
 
         // GET: Item/Details/5
+        //Отображение товара
         public ActionResult Details(int id)
         {
             return View();
         }
 
         // GET: Item/Create
+        //Добавление товара
         public ActionResult Create()
         {
             return View();
         }
 
         // POST: Item/Create
+        //Добавление товара. Запрос в базу данных
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
@@ -41,12 +46,14 @@ namespace RadioMaster.Controllers
         }
 
         // GET: Item/Edit/5
+        //Изменение товара
         public ActionResult Edit(int id)
         {
             return View();
         }
 
         // POST: Item/Edit/5
+        //Изменение товара. Запрос в базу данных
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -64,12 +71,14 @@ namespace RadioMaster.Controllers
         }
 
         // GET: Item/Delete/5
+        //Удаление товара
         public ActionResult Delete(int id)
         {
             return View();
         }
 
         // POST: Item/Delete/5
+        //Удаление товара. Запрос в базу данных
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
