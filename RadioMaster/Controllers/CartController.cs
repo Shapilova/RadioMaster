30946d4d36adaeccb588ab5143ea338094652eed
Fiarcherry <fiarchersschannel@gmail.com>
2019-12-01 18:09:17 +0300

    Контроллер Shop разделён на Cart и Catalog

---
+++ RadioMaster/Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using RadioMaster.Models;
+
+namespace RadioMaster.Controllers
+{
+    //Контроллер корзины
+    public class CartController : Controller
+    {
+        //Отобразить корзину
+        public IActionResult Index()
+        {
+            return View(CartRep.Cart);
+        }
+
+        //Убрать позицию из корзины
+        [HttpGet]
+        public ViewResult RemoveLineFromCart(int idItem)
+        {
+            Item item = CatalogRep.Сatalog
+                .FirstOrDefault(x => x.Id == idItem);
+
+            CartRep.RemoveLine(item);
+
+            ViewBag.ItemType = CatalogRep.Categories;
+            return View("Index", CartRep.Cart);
+        }
+    }
+}
---
+++ RadioMaster/Controllers/CatalogController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using RadioMaster.Models;
+
+namespace RadioMaster.Controllers
+{
+    //Контроллер каталога
+    public class CatalogController : Controller
+    {
+        private static int idFilter;
+
+        public static int GetIdFilter()
+        {
+            return idFilter;
+        }
+
+        public static void SetIdFilter(int value)
+        {
+            idFilter = value;
+        }
+
+        //Отображение каталога
+        public IActionResult Index()
+        {
+            SetIdFilter(0);
+
+            ViewBag.ItemType = CatalogRep.Categories;
+            return View(CatalogRep.Сatalog);
+        }
+
+        //Добавить товар в корзину
+        [HttpGet]
+        public ViewResult AddItemToCart(int idItem)
+        {
+            Item item = CatalogRep.Сatalog
+                .FirstOrDefault(x => x.Id == idItem);
+
+            CartRep.AddItem(item);
+
+            ViewBag.ItemType = CatalogRep.Categories;
+            return ChooseItemsToShowCatalog();
+        }
+
+        //Фильтровать товары по категориям
+        [HttpGet]
+        public ViewResult FilterCatalog(int idItemType)
+        {
+            SetIdFilter(idItemType);
+
+            ViewBag.ItemType = CatalogRep.Categories;
+            return ChooseItemsToShowCatalog();
+        }
+
+        //Выбрать товары для отображения в каталоге
+        private ViewResult ChooseItemsToShowCatalog()
+        {
+            if (GetIdFilter() == 0)
+            {
+                return View("Index", CatalogRep.Сatalog);
+            }
+            else
+            {
+                IEnumerable<Item> items = CatalogRep.Сatalog.Where(x => x.ItemTypeId == GetIdFilter());
+                return View("Index", items);
+            }
+        }
+    }
+}
